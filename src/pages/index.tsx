5471c7e55bc7cc72f1765dd00333216ea88a1b0f
broezer <bruce@moerdjiman.com>
2020-12-10 10:04:44 +0100

    fixed post data import to home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,6 +15,18 @@ import Title from '@/components/Title';
 
 import { getSortedPostsData } from '../../lib/posts';
 
+
+
+interface Post {
+  date: string;
+  title: string;
+  id: string;
+}
+
+interface IProps {
+  allPostsData: Post[];
+} 
+
 export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData()
   return {
@@ -24,15 +36,7 @@ export const getStaticProps: GetStaticProps = async () => {
   }
 }
 
-type allPostsData = {
-  children: {
-    date: string
-    title: string
-    id: string
-  }
-}
-
-const Home: NextPage = ({allPostsData}) => {
+const Home: NextPage<IProps> = ({allPostsData}) => {
   return (
     <>
       <Container>
@@ -41,24 +45,23 @@ const Home: NextPage = ({allPostsData}) => {
           <link href="/favicon.ico" rel="icon" />
         </Head>
         
-        <section>
-         
-          <ul>
-            {allPostsData.map(({ id, date, title }) => (
-              <li  key={id}>
-                {title}
-                <br />
-                {id}
-                <br />
-               
-              </li>
-            ))}
-          </ul>
-        </section>
+        
         <Main>
           <Title>
             Welcome to <a href="https://nextjs.org">Next.js!</a>
           </Title>
+
+          <ul>
+            {allPostsData.map(({ id, date, title }) => (
+              <li key={id}>
+                <Link href={`/posts/${id}`}>
+                  <a>{title}</a>
+                </Link>
+                <br />
+                
+              </li>
+            ))}
+          </ul>
 
           <Description>
             Get started by editing <Code>pages/index.js</Code>
@@ -105,4 +108,6 @@ const Home: NextPage = ({allPostsData}) => {
     </>
   );
 };
+
+
 export default Home;
