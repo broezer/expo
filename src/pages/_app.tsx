5062b088cdce0042a269f16d27a350cc9f734173
broezer <bruce@moerdjiman.com>
2021-01-20 19:36:53 +0100

    allow twitch embed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -46,7 +46,7 @@ export default function App({ Component, pageProps }) {
       <ThemeProvider theme={theme}>
         <Video src="https://player.vimeo.com/external/494532882.hd.mp4?s=ad7ca70b1861433a0ac257cd5965d72db4fd2daf&profile_id=175" />
         
-        <Twitchi src="https://player.twitch.tv/?channel=010bruce&parent=localhost&autoplay=true"></Twitchi>
+        <Twitchi src="https://player.twitch.tv/?channel=010bruce&parent=localhost&parent=expo-di.iuxd.nl&autoplay=true"></Twitchi>
        
         <Component {...pageProps}  />
       
