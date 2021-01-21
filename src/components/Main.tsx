9492f413dfe49d1f7cf176a181032ba1d5b6b7c7
broezer <bruce@moerdjiman.com>
2021-01-21 21:25:21 +0100

    remove livestream & footer

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,14 +1,16 @@
 import styled from 'styled-components';
 
 const Main = styled.main`
-  min-width: 70vw;
-  max-width: 70vw;
+  min-width: 90vw;
+  max-width: 90vw;
   min-height: 80vh;
   background: rgba(0, 0, 0, 0.9);
   border-radius: 10px;
   box-shadow: 0 0 2vw rgb(0, 0, 0);
   margin-top: 5vh;
   margin-bottom: 5vh;
+  margin-left: auto;
+  margin-right: auto;
   padding: 1vw 2vw;
   flex: 1;
   display: flex;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -45,11 +45,9 @@ export default function App({ Component, pageProps }) {
       <GlobalStyle />
       <ThemeProvider theme={theme}>
         <Video src="https://player.vimeo.com/external/494532882.hd.mp4?s=ad7ca70b1861433a0ac257cd5965d72db4fd2daf&profile_id=175" />
-        
-        <Twitchi src="https://player.twitch.tv/?channel=010bruce&parent=localhost&parent=expo-di.iuxd.nl&autoplay=true"></Twitchi>
-        
+                
         <Component {...pageProps}  />
-        <Footer></Footer>
+        
       </ThemeProvider>
     </>
   );
